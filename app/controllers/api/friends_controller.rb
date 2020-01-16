70de6b787a2aff11f20abdac18f38e77c70f8fa3
noel <noelseo@gmail.com>
2020-01-15 17:14:36 -0800

    Revise ActiveRecord for friends_controller.rb

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -1,7 +1,7 @@
 class Api::FriendsController < ApplicationController
 
     def index
-        @friends = User.where.not(id: params[:user_id])
+        @friends = User.where.not(id: params[:user_id]).where.not(email: 'bank@benmo.com')
     end
 
 end
