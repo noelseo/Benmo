bd9036caafbcc0c5f354db731bd55122208afb7c
noel <noelseo@gmail.com>
2020-02-05 10:12:11 -0800

    Add notes to Entities Reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,12 +1,19 @@
 import { combineReducers } from "redux";
 import UsersReducer from "./users_reducer";
+        // ↑ this name
 import TransactionsReducer from "./transactions_reducer";
+        // ↑ this name
 import FriendsReducer from './friends_reducer';
+        // ↑ this name
+        
 
 const EntitiesReducer = combineReducers({
-    users: UsersReducer,
+    users: UsersReducer, 
+           // ↑ this name
     transactions: TransactionsReducer,
+                    // ↑ this name
     friends: FriendsReducer
+                // ↑ this name
 });
 
 export default EntitiesReducer;
