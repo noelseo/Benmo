6b98464a3c8a9bc5287abc1d32e3e9eb77e8d198
noel <noelseo@gmail.com>
2020-02-17 10:55:30 -0800

    Hide add funds option

--- frontend/components/dashboard/profile/profile.jsx
+++ frontend/components/dashboard/profile/profile.jsx
@@ -9,7 +9,8 @@ class Profile extends React.Component {
 
                 <h3 className="profile-fullname">{currentUser.first_name} {currentUser.last_name}</h3>
                 <h6 className="balance">${currentUser.balance.toFixed(2)}</h6>
-                <a href="#" className="funding">Add More Funds »</a>
+                <a href="#" className="funding"></a>
+                {/* <a href="#" className="funding">Add More Funds »</a> */}
 
             </div>
         );
